b8c69938357ace768755c6f1dd5c7b9000240641
Eli Hammond <ebaal8226@gmail.com>
2024-10-02 07:04:09 +0000

    Add products app

--- products/models.py
+++ products/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
+# Create your models here.
 
 class Category(models.Model):
-    class Meta:
-        verbose_name_plural = 'Categories'
-        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -12,7 +10,8 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
-        return self.friendly_name
+        return self.friendly-name
+
 
 
 class Product(models.Model):
--- products/views.py
+++ products/views.py
@@ -1,25 +1,3 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
 
 # Create your views here.
-
-def all_products(request):
-    """ shows individual product details """
-
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-    return render(request, 'products/products.html', context)
-
-
-def product_detail(request, product_id):
-    """ shows all products, including searching and sorting """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-    return render(request, 'products/product_detail.html', context)
