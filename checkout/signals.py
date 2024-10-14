4af8cf1bddd2bea2fddd91eaa8691dc9d35588a6
Eli Hammond <ebaal8226@gmail.com>
2024-10-14 07:19:22 +0000

    Create and connect checkout app

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,21 +1,18 @@
 from django.db.models.signals import post_save, post_delete
-from django.dispatdh import reciever
+from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@reciever(post_save, sender=OrderLineItem)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem save or update
+    Update order total on lineitem update/create
     """
-
     instance.order.update_total()
 
-
-@reciever(post_delete, sender=OrderLineItem)
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_save(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-
     instance.order.update_total()
