09fb73de16663196bcb6213961431adc1ffc7210
Eli Hammond <ebaal8226@gmail.com>
2024-11-11 11:02:54 +0000

    Fix issues in profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -7,29 +7,29 @@ from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 
-# class UserProfile(models.Model):
-#     """
-#     User profile model for maintianing default
-#     delivery informaiton and order history
-#     """
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-#     default_country = CountryField(blank_label='Country *', null=True, blank=True)
-#     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-#     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-#     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-#     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-#     default_county = models.CharField(max_length=80, null=True, blank=True)
+class UserProfile(models.Model):
+    """
+    User profile model for maintianing default
+    delivery informaiton and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
 
-# @receiver(post_save, sender=User)
-# def creeate_or_update_user_profile(sender, instance, created, **kwargs):
-#     """
-#     Create or update user profile
-#     """
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     # Existing users: just save profile
-#     instance.userprofile.save()
+@receiver(post_save, sender=User)
+def creeate_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save profile
+    instance.userprofile.save()
